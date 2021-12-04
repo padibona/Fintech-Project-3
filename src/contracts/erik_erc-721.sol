3829d9cdb0071e68d6555ac47914f01874acf9cb
Erik Larson <eriklarson33@msn.com>
2021-12-04 07:52:11 -0600

    Erik added 1st draft of ERC721 contract & app.py front end.

---
+++ src/contracts/erik_erc-721.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.5.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
+
+contract Character is ERC721Full {
+    constructor() public ERC721Full("Character", "KITC") {}
+
+    struct Character {
+        string name;
+        string class;
+        uint strength;
+        uint agility;
+    }
+
+    mapping(uint => Character) public characters;
+
+    function mint_character(address owner, string memory name, string memory class, uint strength, uint agility, string memory tokenURI)
+        public
+        returns (uint)
+    {
+        uint256 characterId = totalSupply() + 1; //NOTE: totalSupply() is built into ERC-721
+        _mint(owner, characterId);  // mint function takes in the address of where it will be minted and the Token ID.
+        _setTokenURI(characterId, tokenURI);  // Required to mint.  Takes in the Token ID & Token URI.
+
+        return characterId;
+    }
+}
---
+++ src/pages/erik_app.py
@@ -0,0 +1,71 @@
+import os
+import json
+from web3 import Web3
+from pathlib import Path
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
+
+# Define and connect a new Web3 provider
+w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
+
+################################################################################
+# Contract Helper function:
+# 1. Loads the contract once using cache
+# 2. Connects to the contract using the contract address and ABI
+################################################################################
+
+# Cache the contract on load
+@st.cache(allow_output_mutation=True)
+# Define the load_contract function
+def load_contract():
+
+    # Load Art Gallery ABI
+    with open(Path('./contracts/compiled/certificate_abi.json')) as f:
+        certificate_abi = json.load(f)
+
+    # Set the contract address (this is the address of the deployed contract)
+    contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
+
+    # Get the contract
+    contract = w3.eth.contract(
+        address=contract_address,
+        abi=certificate_abi
+    )
+    # Return the contract from the function
+    return contract
+
+
+# Load the contract
+contract = load_contract()
+
+
+################################################################################
+# Award Certificate
+################################################################################
+
+# accounts = w3.eth.accounts
+# account = accounts[0]
+# student_account = st.selectbox("Select Account", options=accounts)
+# certificate_details = st.text_input("Certificate Details", value="FinTech Certificate of Completion")
+# if st.button("Award Certificate"):
+#     contract.functions.awardCertificate(student_account, certificate_details).transact({'from': account, 'gas': 1000000})
+
+################################################################################
+# Display Certificate
+################################################################################
+characterId = st.number_input("Enter a Certificate Token ID to display", value=0, step=1)
+if st.button("Display Character"):
+    # Get the certificate owner
+    character_owner = contract.functions.ownerOf(characterId).call()
+    st.write(f"The certificate was awarded to {character_owner}")
+
+    # Get the certificate's metadata
+    character_uri = contract.functions.tokenURI(characterId).call()
+    st.write(f"The certificate's tokenURI metadata is {character_uri}")
+
+
+################################################################################
+# Mint New Character
+################################################################################

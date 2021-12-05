171e351b6040b5aa9ff94369b39b8b42f04e5a7f
Peter DiBona <padibona@gmail.com>
2021-12-05 14:40:17 -0600

    More cleanup

--- src/pages/app.py
+++ src/pages/app.py
@@ -14,26 +14,18 @@ PAGES = {
 
 def main():
     """Main function of the App"""
-    st.sidebar.title("Navigation")
+    st.sidebar.title("Navigation - Choose your application you want to work with")
     selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 
     page = PAGES[selection]
 
     # with st.spinner(f"Loading {selection} ..."):
     #     ast.shared.components.write_page(page)
-    st.sidebar.title("Contribute")
-    st.sidebar.info(
-        "This an open source project and you are very welcome to **contribute** your awesome "
-        "comments, questions, resources and apps as "
-        "[issues](https://github.com/MarcSkovMadsen/awesome-streamlit/issues) of or "
-        "[pull requests](https://github.com/MarcSkovMadsen/awesome-streamlit/pulls) "
-        "to the [source code](https://github.com/MarcSkovMadsen/awesome-streamlit). "
-    )
+
     st.sidebar.title("About")
     st.sidebar.info(
         """
-        This app is maintained by Marc Skov Madsen. You can learn more about me at
-        [datamodelsanalytics.com](https://datamodelsanalytics.com).
+        This app was created by Team 1 of the Fintech Bootcamp.
 """
     )
 
